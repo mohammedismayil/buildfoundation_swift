9e8f106eae21626012a23124b29aeee6eea15447
Ismaaayil <mohammedismayil10@gmail.com>
2023-02-24 23:14:00 +0100

    Debugging spring animation

--- BuildFoundation/CASpringAnimationTestVC.swift
+++ BuildFoundation/CASpringAnimationTestVC.swift
@@ -41,11 +41,11 @@ class CASpringAnimationTestVC: UIViewController {
         self.navigationController?.isNavigationBarHidden = true
         self.view.backgroundColor = .white
         self.view.addSubview(ball1View)
-//        self.view.addSubview(animateBtn)
+        //        self.view.addSubview(animateBtn)
         self.view.addSubview(slider)
         
         
-
+        
         self.ball1View.frame = CGRect(x: self.view.center.x - 10, y: self.view.center.y  , width: 20, height: 20)
         self.animateBtn.frame = CGRect(x: self.view.center.x - 75, y: self.view.center.y + 100  , width: 100, height: 50)
         
@@ -66,30 +66,30 @@ class CASpringAnimationTestVC: UIViewController {
         pulse.initialVelocity = 0.1
         pulse.damping = 10
         
-//        ball1View.layer.add(pulse, forKey: "pulse")
+        //        ball1View.layer.add(pulse, forKey: "pulse")
         
-       
-
+        
+        
         for i in 1..<10 {
-//            ball1View.transform = CGAffineTransform(scaleX:0.2 * CGFloat(i), y: 0.2 * CGFloat(i))
+            //            ball1View.transform = CGAffineTransform(scaleX:0.2 * CGFloat(i), y: 0.2 * CGFloat(i))
         }
         
         
         UIView.animate(withDuration: 1, delay: 0) {
-
-//            switch self.currentAnimation {
-//            case 0 :
-//                self.ball1View.transform = CGAffineTransform(scaleX: 2, y: 2)
-//
-//            case 1 :
-//                self.ball1View.layer.add(pulse, forKey: "pulse")
-//                self.ball1View.transform = .identity
-//            default:
-//                break
-//            }
+            
+            //            switch self.currentAnimation {
+            //            case 0 :
+            //                self.ball1View.transform = CGAffineTransform(scaleX: 2, y: 2)
+            //
+            //            case 1 :
+            //                self.ball1View.layer.add(pulse, forKey: "pulse")
+            //                self.ball1View.transform = .identity
+            //            default:
+            //                break
+            //            }
             
             self.ball1View.transform = CGAffineTransform(scaleX: CGFloat(1 * self.currentAnimation), y: CGFloat(1 * self.currentAnimation))
-
+            
         }
         
         currentAnimation += 1
@@ -99,24 +99,33 @@ class CASpringAnimationTestVC: UIViewController {
     @objc func sliderValueChanged() {
         
         print(slider.value  )
-        
-        UIView.animate(withDuration: 1, delay: 0) {
-            if self.slider.value == 1 {
-                self.ball1View.transform = .identity
-                let pulse = CASpringAnimation(keyPath: "transform.scale")
-                pulse.duration = 1
-                pulse.fromValue = 1
-                pulse.toValue = 1.5
-                pulse.autoreverses = false
-                pulse.repeatCount = 1
-                pulse.initialVelocity = 0.1
-                pulse.damping = 1
-                self.ball1View.layer.add(pulse, forKey: "pulse")
-            }else{
-                self.ball1View.transform = CGAffineTransform(scaleX: CGFloat(self.slider.value * 2) , y: CGFloat(self.slider.value * 2))
-            }
-           
-
-        }
+        let pulse = CASpringAnimation(keyPath: "transform.scale")
+        pulse.duration = 0.4
+        pulse.fromValue = 1.1
+        pulse.toValue = 1
+        pulse.autoreverses = false
+        pulse.repeatCount = 1
+        pulse.initialVelocity = 0.5
+        pulse.damping = 1.0
+        self.ball1View.layer.add(pulse, forKey: "pulse")
+        //        UIView.animate(withDuration: 2, delay: 0) {
+        //            if self.slider.value == 1 {
+        //                self.ball1View.transform = .identity
+        //                let pulse = CASpringAnimation(keyPath: "transform.scale")
+        //                pulse.duration = 0.2
+        //                pulse.fromValue = 1.05
+        //                pulse.toValue = 1.0
+        //                pulse.autoreverses = false
+        //                pulse.repeatCount = 1
+        //                pulse.initialVelocity = 0.5
+        //                pulse.damping = 1.0
+        //
+        //                self.ball1View.layer.add(pulse, forKey: "pulse")
+        //            }else{
+        //                self.ball1View.transform = CGAffineTransform(scaleX: CGFloat(self.slider.value * 2) , y: CGFloat(self.slider.value * 2))
+        //            }
+        //
+        //
+        //        }
     }
 }
