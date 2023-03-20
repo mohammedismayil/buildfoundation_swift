0d79b82eee851d57a525957078ca9f7adb6b88f1
Ismaaayil <mohammedismayil10@gmail.com>
2023-03-20 07:07:39 +0530

    Unwanted delegate removed

--- BuildFoundation/ViewControllerTransition/ViewControllerTransitionManager.swift
+++ BuildFoundation/ViewControllerTransition/ViewControllerTransitionManager.swift
@@ -21,18 +21,13 @@ class ViewControllerTransitionManager: NSObject,UIViewControllerTransitioningDel
         animator = ViewControllerTransition(type: .present, delegate: delegate)
             return animator
         }
-
-        // 3
         func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
  
             return nil
         }
     
 }
-final class ViewControllerTransition: NSObject, UIViewControllerAnimatedTransitioning {
-    
-    // 9
-    
+class ViewControllerTransition: NSObject, UIViewControllerAnimatedTransitioning {
     static let duration: TimeInterval = 4
     
     private let type: PresentationType
@@ -43,65 +38,17 @@ final class ViewControllerTransition: NSObject, UIViewControllerAnimatedTransiti
         self.type = type
         self.delegate = delegate
     }
-    
-    // 12
+ 
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return Self.duration
     }
-    
-    // 13
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
      
-        let containerView = transitionContext.containerView
-       
-       
-        
-        guard
-            let vc = transitionContext.viewController(forKey: .from) as? TransitionCollectionViewViewController,
-            let window = vc.view.window,
-            let vc2 = transitionContext.viewController(forKey: .to) as? TransitionCollectionViewViewController2,
-            let toView = vc2.view,
-            let cellImageSnapshot = delegate?.snapShotView().snapshotView(afterScreenUpdates: true),
-            let controllerImageSnapshot = vc2.locationImageView.snapshotView(afterScreenUpdates: true),
-            let cellImageViewRect =
-                delegate?.snapShotView().convert(delegate?.snapShotView().bounds ?? CGRect(), to: window)
-                
-        else {
-            transitionContext.completeTransition(true)
-            return
-        }
-        
-        containerView.addSubview(toView)
-        let backgroundView: UIView
-        let fadeView = UIView(frame: containerView.bounds)
-        fadeView.backgroundColor = vc2.view.backgroundColor
-        backgroundView = UIView(frame: containerView.bounds)
-        backgroundView.addSubview(fadeView)
-        fadeView.alpha = 0
-        toView.alpha = 0
-        containerView.addSubview(backgroundView)
-        containerView.addSubview(cellImageSnapshot)
-        containerView.addSubview(controllerImageSnapshot)
-       
-        let controllerImageViewRect = vc2.locationImageView.convert(vc2.locationImageView.bounds, to: window)
-        
-        
-        cellImageSnapshot.frame = cellImageViewRect
-        controllerImageSnapshot.frame = cellImageViewRect
-        controllerImageSnapshot.alpha = 0
-        cellImageSnapshot.alpha = 1
-        performAnimation(duration: Self.duration) {
-            cellImageSnapshot.alpha = 0
-            controllerImageSnapshot.alpha = 1
-            cellImageSnapshot.frame =  controllerImageViewRect
-            controllerImageSnapshot.frame =  controllerImageViewRect
-            fadeView.alpha = 1
-        } completion: { (success) in
-            cellImageSnapshot.removeFromSuperview()
-            controllerImageSnapshot.removeFromSuperview()
-            backgroundView.removeFromSuperview()
-            toView.alpha = 1
-            transitionContext.completeTransition(true)
+        switch type {
+        case .present:
+            self.presentAnimation(using: transitionContext)
+        case .dismiss:
+            self.dismissAnimation(using: transitionContext)
         }
         
     }
@@ -120,8 +67,102 @@ final class ViewControllerTransition: NSObject, UIViewControllerAnimatedTransiti
         
     }
     
-    func dismissAnimation(using transitionContext: UIViewControllerContextTransitioning?) {
+    func presentAnimation(using transitionContext: UIViewControllerContextTransitioning) {
         
+        let containerView = transitionContext.containerView
+        guard
+            let vc = transitionContext.viewController(forKey: .from) as? TransitionCollectionViewViewController,
+            let window = vc.view.window,
+            let vc2 = transitionContext.viewController(forKey: .to) as? TransitionCollectionViewViewController2,
+            let toView = vc2.view,
+            let cellImageSnapshot = delegate?.snapShotView().snapshotView(afterScreenUpdates: true),
+            let controllerImageSnapshot = vc2.locationImageView.snapshotView(afterScreenUpdates: true),
+            let cellImageViewRect =
+                delegate?.snapShotView().convert(delegate?.snapShotView().bounds ?? CGRect(), to: window)
+                
+        else {
+            transitionContext.completeTransition(true)
+            return
+        }
+        let controllerImageViewRect = vc2.locationImageView.convert(vc2.locationImageView.bounds, to: window)
+             
+        containerView.addSubview(toView)
+        let backgroundView: UIView
+        let fadeView = UIView(frame: containerView.bounds)
+        fadeView.backgroundColor = vc2.view.backgroundColor
+        backgroundView = UIView(frame: containerView.bounds)
+        backgroundView.addSubview(fadeView)
+        fadeView.alpha = 0
+        toView.alpha = 0
+        containerView.addSubview(backgroundView)
+        containerView.addSubview(cellImageSnapshot)
+        containerView.addSubview(controllerImageSnapshot)
+        cellImageSnapshot.frame = cellImageViewRect
+        cellImageSnapshot.alpha = 1
+        controllerImageSnapshot.frame = cellImageViewRect
+        controllerImageSnapshot.alpha = 0
+        performAnimation(duration: Self.duration) {
+            cellImageSnapshot.alpha = 0
+            cellImageSnapshot.frame =  controllerImageViewRect
+            controllerImageSnapshot.alpha = 1
+            controllerImageSnapshot.frame =  controllerImageViewRect
+            fadeView.alpha = 1
+        } completion: { (success) in
+            cellImageSnapshot.removeFromSuperview()
+            controllerImageSnapshot.removeFromSuperview()
+            backgroundView.removeFromSuperview()
+            toView.alpha = 1
+            transitionContext.completeTransition(true)
+        }
+    }
+    
+    func dismissAnimation(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        let containerView = transitionContext.containerView
+        guard
+            let vc = transitionContext.viewController(forKey: .from) as? TransitionCollectionViewViewController,
+            let window = vc.view.window,
+            let vc2 = transitionContext.viewController(forKey: .to) as? TransitionCollectionViewViewController2,
+            let toView = vc2.view,
+            let cellImageSnapshot = delegate?.snapShotView().snapshotView(afterScreenUpdates: true),
+            let controllerImageSnapshot = vc2.locationImageView.snapshotView(afterScreenUpdates: true),
+            let cellImageViewRect =
+                delegate?.snapShotView().convert(delegate?.snapShotView().bounds ?? CGRect(), to: window)
+                
+        else {
+            transitionContext.completeTransition(true)
+            return
+        }
+        let controllerImageViewRect = vc2.locationImageView.convert(vc2.locationImageView.bounds, to: window)
+             
+        containerView.addSubview(toView)
+        let backgroundView: UIView
+        let fadeView = UIView(frame: containerView.bounds)
+        fadeView.backgroundColor = vc2.view.backgroundColor
+        backgroundView = UIView(frame: containerView.bounds)
+        backgroundView.addSubview(fadeView)
+        fadeView.alpha = 0
+        toView.alpha = 0
+        containerView.addSubview(backgroundView)
+        containerView.addSubview(cellImageSnapshot)
+        containerView.addSubview(controllerImageSnapshot)
+        cellImageSnapshot.frame = cellImageViewRect
+        cellImageSnapshot.alpha = 1
+        controllerImageSnapshot.frame = cellImageViewRect
+        controllerImageSnapshot.alpha = 0
+        performAnimation(duration: Self.duration) {
+            cellImageSnapshot.alpha = 0
+            cellImageSnapshot.frame =  controllerImageViewRect
+            controllerImageSnapshot.alpha = 1
+            controllerImageSnapshot.frame =  controllerImageViewRect
+            fadeView.alpha = 1
+        } completion: { (success) in
+            cellImageSnapshot.removeFromSuperview()
+            controllerImageSnapshot.removeFromSuperview()
+            backgroundView.removeFromSuperview()
+            toView.alpha = 1
+            transitionContext.completeTransition(true)
+        }
     }
 }
 enum PresentationType {
