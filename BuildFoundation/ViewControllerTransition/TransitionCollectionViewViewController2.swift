2105f72b61f937cefc0293e6e4d2c34c7d114791
Ismaaayil <mohammedismayil10@gmail.com>
2023-03-20 08:03:06 +0530

    Transition2 Vc collection view added

--- BuildFoundation/ViewControllerTransition/TransitionCollectionViewViewController2.swift
+++ BuildFoundation/ViewControllerTransition/TransitionCollectionViewViewController2.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class TransitionCollectionViewViewController2: UIViewController {
+    
+    
     var data: TransitionCellData!
 
-    @IBOutlet private(set) var locationImageView: UIImageView!
-    @IBOutlet private(set) var locationLabel: UILabel!
+    @IBOutlet weak var transition2CV: UICollectionView!
+   
     @IBOutlet private(set) var closeButton: UIButton!
 
     override func viewDidLoad() {
@@ -21,14 +23,46 @@ class TransitionCollectionViewViewController2: UIViewController {
         let cross = #imageLiteral(resourceName: "cross").withRenderingMode(.alwaysTemplate)
         closeButton.setImage(cross, for: .normal)
         closeButton.tintColor = .white
+        
+//        transition2CV.register(Transition2CVC.self, forCellWithReuseIdentifier: "Transition2CVC")
+        transition2CV.delegate = self
+        transition2CV.dataSource = self
+        transition2CV.reloadData()
+        
 
-        locationImageView.image = data.image
-        locationLabel.text = data.title
-        locationImageView.layer.cornerRadius = 50
+      
     }
 
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true)
     }
 
+}
+
+extension TransitionCollectionViewViewController2: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Transition2CVC", for: indexPath) as! Transition2CVC
+//        cell.configure(data: data)
+        cell.locationImageView.image = data.image
+        cell.locationLabel.text = data.title
+        cell.locationImageView.layer.cornerRadius = 50
+        return cell
+    }
+    
+    
+}
+
+class Transition2CVC: UICollectionViewCell{
+    
+    @IBOutlet var locationImageView: UIImageView!
+    @IBOutlet var locationLabel: UILabel!
+    
+   
 }
