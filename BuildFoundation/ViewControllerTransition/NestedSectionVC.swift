a4d9b6eab5a04924c365cbe6e6d15eecaa08dbf2
Ismaaayil <mohammedismayil10@gmail.com>
2023-05-01 14:04:21 +0530

    Nested sections completed

--- BuildFoundation/ViewControllerTransition/NestedSectionVC.swift
+++ BuildFoundation/ViewControllerTransition/NestedSectionVC.swift
@@ -25,7 +25,7 @@ class NestedSectionVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var sections:[Section] = [Section(title: "Sports", subSection: [SubSection(title: "Bat-and-ball", row: [Row(title: "Baseball"),Row(title: "Softball"),Row(title: "Cricket")])]),
+    var sections:[Section] = [Section(title: "Sports", subSection: [SubSection(title: "Bat-and-ball", row: [Row(title: "Baseball"),Row(title: "Softball"),Row(title: "Cricket")]),SubSection(title: "Hockey", row: [Row(title: " Field Hockey"),Row(title: "Roller Hockey"),Row(title: "Ice Hockey")])]),
                               Section(title: "Engineering", subSection: [SubSection(title: "Science", row: [Row(title: "Baseball"),Row(title: "Softball"),Row(title: "Cricket")])])]
     
     override func viewDidLoad() {
@@ -55,13 +55,36 @@ class NestedSectionVC: UIViewController {
 
 extension NestedSectionVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sections[section].subSection.count
+        let sectionItems = sections[section]
+        var numberOfRows: Int = sectionItems.subSection.count // For second level section headers
+        for rowItems in 0..<sectionItems.subSection.count  {
+            numberOfRows += sectionItems.subSection.count // For actual table rows
+           }
+           return numberOfRows
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "NestedTableViewCell", for: indexPath) as! NestedTableViewCell
-        cell.titleLabel.text = "T"
-        return cell
+        var sectionItems = sections[indexPath.section]
+            var sectionHeaders = self.sections[indexPath.section]
+            let itemAndSubsectionIndex: IndexPath? = computeItemAndSubsectionIndex(for: indexPath)
+            let subsectionIndex = Int(itemAndSubsectionIndex?.section ?? 0)
+            let itemIndex: Int? = itemAndSubsectionIndex?.row
+
+            if (itemIndex ?? 0) < 0 {
+                // Section header
+                let cell = tableView.dequeueReusableCell(withIdentifier: "NestedSubSectionTableViewCell", for: indexPath) as! NestedSubSectionTableViewCell
+                cell.backgroundColor = .red
+                cell.titleLabel.text = sections[indexPath.section].subSection[subsectionIndex ?? 0].title
+                return cell
+            } else {
+                // Row Item
+                let cell = tableView.dequeueReusableCell(withIdentifier: "NestedTableViewCell", for: indexPath) as! NestedTableViewCell
+                cell.backgroundColor = .green
+                cell.titleLabel.text = sections[indexPath.section].subSection[subsectionIndex ?? 0].row[itemIndex ?? 0].title
+                return cell
+                
+            }
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -83,11 +106,36 @@ extension NestedSectionVC: UITableViewDelegate, UITableViewDataSource {
         return sectionView
     }
     
+    func computeItemAndSubsectionIndex(for indexPath: IndexPath?) -> IndexPath? {
+        var sectionItems = sections[Int(indexPath?.section ?? 0)]
+        var itemIndex: Int? = indexPath?.row
+        var subsectionIndex: Int = 0
+        for i in 0..<sectionItems.subSection.count {
+            // First row for each section item is header
+            itemIndex = (itemIndex ?? 0) - 1
+            // Check if the item index is within this subsection's items
+            let subsectionItems = sectionItems
+            if (itemIndex ?? 0) < Int(subsectionItems.subSection.count ?? 0) {
+                subsectionIndex = i
+                break
+            } else {
+                itemIndex! -= subsectionItems.subSection.count ?? 0
+            }
+        }
+        return IndexPath(row: itemIndex ?? 0, section: subsectionIndex)
+    }
+    
     
 }
 
 
 class NestedTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel:UILabel!
+}
+
+
+class NestedSubSectionTableViewCell: UITableViewCell {
     
     @IBOutlet weak var titleLabel:UILabel!
 }
