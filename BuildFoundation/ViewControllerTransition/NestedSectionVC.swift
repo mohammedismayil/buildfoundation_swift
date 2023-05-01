a4606870ef16b22c57ec9f1c242ded6a04db06c5
Ismaaayil <mohammedismayil10@gmail.com>
2023-05-01 12:38:41 +0530

    Nested section vc added

---
+++ BuildFoundation/ViewControllerTransition/NestedSectionVC.swift
@@ -0,0 +1,35 @@
+//
+//  NestedSectionVC.swift
+//  BuildFoundation
+//
+//  Created by ismayil-16441 on 01/05/23.
+//
+
+import Foundation
+import UIKit
+
+class NestedSectionVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+       
+        
+        
+    }
+  
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
