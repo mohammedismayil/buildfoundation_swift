5f712349e51c466deb274b30db2498f9dae51a26
Ismaaayil <mohammedismayil10@gmail.com>
2023-05-01 13:13:13 +0530

    Nested sections added

--- BuildFoundation/ViewControllerTransition/NestedSectionVC.swift
+++ BuildFoundation/ViewControllerTransition/NestedSectionVC.swift
@@ -8,15 +8,34 @@
 import Foundation
 import UIKit
 
+
+
+struct Section {
+    var title:String
+    var subSection:[SubSection]
+}
+struct SubSection {
+    var title:String
+    var row:[Row]
+}
+struct Row {
+    var title:String
+}
 class NestedSectionVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var sections:[Section] = [Section(title: "Sports", subSection: [SubSection(title: "Bat-and-ball", row: [Row(title: "Baseball"),Row(title: "Softball"),Row(title: "Cricket")])]),
+                              Section(title: "Engineering", subSection: [SubSection(title: "Science", row: [Row(title: "Baseball"),Row(title: "Softball"),Row(title: "Cricket")])])]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
         
         
     }
@@ -32,4 +51,43 @@ class NestedSectionVC: UIViewController {
     }
     */
 
+}
+
+extension NestedSectionVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].subSection.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NestedTableViewCell", for: indexPath) as! NestedTableViewCell
+        cell.titleLabel.text = "T"
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+
+        let sectionView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 25))
+        sectionView.backgroundColor = UIColor.magenta
+
+        let sectionName = UILabel(frame: CGRect(x: 5, y: 0, width: tableView.frame.size.width, height: 25))
+        sectionName.text = sections[section].title
+        sectionName.textColor = UIColor.white
+        sectionName.font = UIFont.systemFont(ofSize: 14)
+        sectionName.textAlignment = .left
+
+        sectionView.addSubview(sectionName)
+        return sectionView
+    }
+    
+    
+}
+
+
+class NestedTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel:UILabel!
 }
